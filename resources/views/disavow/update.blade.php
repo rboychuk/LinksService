5f6f63ae442e39619dda548bb5450e583c6a9247
beaver <alex101ki@gmail.com>
2021-03-31 13:29:34 +0300

    Fixed disavow Service

--- resources/views/disavow/update.blade.php
+++ resources/views/disavow/update.blade.php
@@ -3,13 +3,25 @@
 
 @section('content')
 
+    <style>
+        .p_comment{
+            font-style: italic;
+            color: grey;
+        }
+        .p_comment strong {
+            font-size: 14pt;
+            width: 50px;
+            display: inline-block;
+        }
+    </style>
+
     <div class="container">
-        <p>Ahrefs <strong>{{ count($ahrefs_links) }}</strong></p>
-        <p>Google <strong>{{ count($google_links) }}</strong></p>
-        <p>Disavow <strong>{{ count($disavow_links) }}</strong></p>
-        <p>Our Domains <strong>{{ count($domains) }}</strong></p>
-        <p>Our Checked Good domains <strong>{{ count($disavow_domains) }}</strong></p>
-        <p>New Disavow <strong>{{ count($diff) }}</strong></p>
+        <p class="p_comment"><strong>{{ count($google_links) }}</strong> Ссылки которые вы загрузили из файла Google Domains</p>
+        <p class="p_comment"><strong>{{ count($ahrefs_links) }}</strong> Ссылки которые вы загрузили из файла Ahref Domains</p>
+        <p class="p_comment"><strong>{{ count($disavow_links) }}</strong> Ссылки которые вы загрузили из файла Disavow</p>
+        <p class="p_comment"><strong>{{ count($domains) }}</strong> Количество уникальных доменов</p>
+        <p class="p_comment"><strong>{{ count($disavow_domains) }}</strong> Disavow домены, которые были добавлены вручную</p>
+        <p class="p_comment"><strong>{{ count($diff) }}</strong> Новые домены (из google доменов, которых нет в ahrefs,disavow,domains,disavov domains)</p>
         <div class="row">
         </div>
         <div class="row">
